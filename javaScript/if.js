8712e7c00f53abbecc050d6ca058e6da34e7e0bb
pinyo1999 <pinyo.sum@ku.th>
2021-09-18 00:02:01 +0700

    no message

---
+++ javaScript/if.js
@@ -0,0 +1,9 @@
+let score = 69
+
+if (score >= 80) {
+    console.log("A")
+} else if (score >= 70) {
+    console.log("B")
+} else {
+    console.log("F")
+}
---
+++ javaScript/index.js
@@ -0,0 +1,44 @@
+// การประกาศตัวแปร
+
+let x = 2
+let y = 10
+let z = x + y
+console.log(z)
+
+// ชนิดข้อมูล
+
+let number = 2
+let string = "pinyo"
+let boolean = false // true, false
+let object = { firstName: "John", lastName: "Doe" };    // Object
+
+
+// string => number
+
+let num = number("12")
+
+// number => string
+
+let str = string(12)
+
+
+// Operator
+
+// +	Addition
+// -	Subtraction
+// *	Multiplication
+// **	Exponentiation (ES2016)
+// /	Division
+// %	Modulus (Division Remainder)
+// ++	Increment
+// --	Decrement
+
+
+// =	x = y	x = y
+// +=	x += y	x = x + y
+// -=	x -= y	x = x - y
+// *=	x *= y	x = x * y
+// /=	x /= y	x = x / y
+// %=	x %= y	x = x % y
+// **=	x **= y	x = x ** y
+
